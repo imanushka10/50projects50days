52e66e6450e8e78e1c4dd99e1d6362263e400076
Anu Singh <anusingh1883656@gmail.com>
2020-12-09 12:01:58 +0530

    scroll-animation

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
+const boxes = document.querySelectorAll('.box')
 
-let load = 0
-let int = setInterval(blurring, 30)
+window.addEventListener('scroll', checkBoxes)
 
+checkBoxes() // it is declaration of defining a named function
 
-function blurring() {
-    load++
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4
 
-    if (load > 99) {
-        clearInterval(int)
-    }
-    loadText.innerText = `${load}%`
-    loadText.style.opacity = scale(load, 0, 100, 1, 0)
-    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
-}
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
 
-
-
-// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
-const scale = (num, in_min, in_max, out_min, out_max) => {
-    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+        if (boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
 }
