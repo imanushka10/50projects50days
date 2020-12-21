6079c36dcde1a77a51a7a9b66c7321bfd130ca17
Anu Singh <anusingh1883656@gmail.com>
2020-12-21 11:34:34 +0530

    drink-water

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
-const counters = document.querySelectorAll('.counter')
+const smallCups = document.querySelectorAll(".cup-small")
+const listers = document.getElementById("liters")
+const percentage = document.getElementById('percentage')
+const remained = document.getElementById('remained')
 
+updateBigCup()
 
-counters.forEach(counter => {
-    counter.innerHTML = "0"
+smallCups.forEach((cup, idx) => {
+    cup.addEventListener('click', () => highlightCups(idx))
+})
 
-    function updateCounter() {
-        const target = +counter.getAttribute('data-target')
-        const c = +counter.innerText
-        const increment = target / 200
-        if (c < target) {
-            counter.innerText = `${Math.ceil(c + increment)}`
-            setTimeout(updateCounter, 1)
+function highlightCups(idx) {
+
+    if (smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+        idx--
+    }
+
+    smallCups.forEach((cup, idx2) => {
+        if (idx2 <= idx) {
+            cup.classList.add('full')
         }
         else {
-            counter.innerText = target
-        }
-    }
-    updateCounter()
+            cup.classList.remove('full')
 
-})
+        }
+    })
+    updateBigCup()
+}
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    const totalCups = smallCups.length
+
+    if (fullCups === 0) {
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    }
+    else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+        percentage.innerText = `${fullCups / totalCups * 100}%`
+    }
+    if (fullCups === totalCups) {
+        remained.style.visibility = 'hidden'
+        remained.style.height = 0
+    }
+    else {
+
+        remained.style.visibility = 'visible'
+        listers.innerText = `${2 - (250 * fullCups / 1000)}L`
+    }
+}
